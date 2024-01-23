c6e715ef22dcfe018e04f0e37fe67bec6b041353
manuel220104 <marangog3eafit.edu.co>
2024-01-23 15:36:09 -0500

    Changes in the vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite'; // Add this line
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@fortawesome/react-fontawesome'],
-    },
-  },
 });
