98a004bf8ebd5ca795c3c9625a5510421e90c617
manuel220104 <marangog3eafit.edu.co>
2024-02-07 10:17:49 -0500

    Add the Acentos repository Link

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,7 +13,8 @@ export function Projects() {
                     <div>
                         <h2 className="Subtittle-Project">Ecommerce - AcentosApp</h2>
                         <h3 className="Text-Projects">Ever wanted to explore the fantastic world of books at EAFIT University's Acentos Library? 📚 But wait, there's no online platform to make your book-hunting easier! 😩 Fret not! We're here to save the day with a brilliant web application. 🌐💻 With AcentosPage, you can dive into the treasure trove of discounted books and materials, browse, buy, and have them delivered right to your doorstep! 🚚📦</h3>
-                        <a className="Link" href="https://acentos.dis.eafit.edu.co/">Link AcentosApp</a>
+                        <a className="Link" href="https://acentos.dis.eafit.edu.co/">AcentosApp</a>
+                        <a className="Link" href="https://github.com/Manuel220104/Integrative-Project-1">Github Repository</a>
                         <img className="Project-Image" src={Acentos}/>
                         
                     </div>
